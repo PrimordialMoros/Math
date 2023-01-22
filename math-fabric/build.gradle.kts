3290d9d7612b4563b086eb58942fc03f6ed3dea8
PrimordialMoros <mail@moros.me>
2023-01-22 19:12:39 +0200

    Update fabric impl

--- math-fabric/build.gradle.kts
+++ math-fabric/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.fabric.loom)
 }
 
+version = "${libs.versions.minecraft.get()}-${rootProject.version}"
+
 repositories {
     maven("https://maven.fabricmc.net/")
 }
@@ -12,4 +14,10 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modCompileOnly(libs.fabric.api)
     modCompileOnly(libs.fabric.loader)
+}
+
+tasks.named<Copy>("processResources") {
+    filesMatching("fabric.mod.json") {
+        expand("version" to rootProject.version)
+    }
 }
